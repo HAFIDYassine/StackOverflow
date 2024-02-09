01017d2b4666fd5309b4ea18ec2203367c248baa
Riane SMILI <rianesmili@gmail.com>
2024-02-09 16:31:54 +0100

    TP5 Add SnackbarHostState

--- app/src/main/java/com/example/stackoverflow/QuestionResponse.kt
+++ app/src/main/java/com/example/stackoverflow/QuestionResponse.kt
@@ -3,11 +3,11 @@ package com.example.stackoverflow
 import com.example.stackoverflow.data.Question
 
 sealed interface QuestionResponse {
+
     object Pending : QuestionResponse
-
-
     @JvmInline
     value class Success(val list: List<Question>) : QuestionResponse
+    data class Error(val message: String) : QuestionResponse
 
     @JvmInline
     value class Error(val message: String) : QuestionResponse
